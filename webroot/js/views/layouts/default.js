4f94d7b4aa4b21e56cb6a6a3753e822a8ef74d7c
Niles <neterslandreau@gmail.com>
2011-05-20 16:17:52 -0400

    improvments to carousel.

--- webroot/js/views/layouts/default.js
+++ webroot/js/views/layouts/default.js
@@ -18,6 +18,25 @@ $(document).ready(function() {
 		auto: 1,
 		size: 10,
 		wrap: 'circular',
-		animation: 2000
+		animation: 2000,
+		initCallback: mycarousel_initCallback
 	});
 });
+
+function mycarousel_initCallback(carousel) {
+    // Disable autoscrolling if the user clicks the prev or next button.
+    carousel.buttonNext.bind('click', function() {
+        carousel.startAuto(0);
+    });
+
+    carousel.buttonPrev.bind('click', function() {
+        carousel.startAuto(0);
+    });
+
+    // Pause autoscrolling if the user moves with the cursor over the clip.
+    carousel.clip.hover(function() {
+        carousel.stopAuto();
+    }, function() {
+        carousel.startAuto();
+    });
+};
