2ff9ca4c8544b9db4f1efed767fb4e85bb502fcf
Niles <neterslandreau@gmail.com>
2011-06-13 11:39:14 -0400

    change cakephp core to use 2.0.

--- webroot/index.php
+++ webroot/index.php
@@ -51,7 +51,7 @@
  *
  */
 	if (!defined('CAKE_CORE_INCLUDE_PATH')) {
-		define('CAKE_CORE_INCLUDE_PATH',  DS . 'usr' . DS . 'share' . DS . 'cakephp');
+		define('CAKE_CORE_INCLUDE_PATH',  DS . 'usr' . DS . 'share' . DS . 'cakephp2' . DS . 'cakephp' . DS . 'lib');
 	}
 
 /**
@@ -66,20 +66,17 @@
 		define('WWW_ROOT', dirname(__FILE__) . DS);
 	}
 	if (!defined('CORE_PATH')) {
-		if (function_exists('ini_set') && ini_set('include_path', CAKE_CORE_INCLUDE_PATH . PATH_SEPARATOR . ROOT . DS . APP_DIR . DS . PATH_SEPARATOR . ini_get('include_path'))) {
-			define('APP_PATH', null);
-			define('CORE_PATH', null);
-		} else {
-			define('APP_PATH', ROOT . DS . APP_DIR . DS);
-			define('CORE_PATH', CAKE_CORE_INCLUDE_PATH . DS);
-		}
+		define('APP_PATH', ROOT . DS . APP_DIR . DS);
+		define('CORE_PATH', CAKE_CORE_INCLUDE_PATH . DS);
 	}
-	if (!include(CORE_PATH . 'cake' . DS . 'bootstrap.php')) {
+	if (!include(CORE_PATH . 'Cake' . DS . 'bootstrap.php')) {
 		trigger_error("CakePHP core could not be found.  Check the value of CAKE_CORE_INCLUDE_PATH in APP/webroot/index.php.  It should point to the directory containing your " . DS . "cake core directory and your " . DS . "vendors root directory.", E_USER_ERROR);
 	}
-	if (isset($_GET['url']) && $_GET['url'] === 'favicon.ico') {
+
+	if (isset($_SERVER['PATH_INFO']) && $_SERVER['PATH_INFO'] == '/favicon.ico') {
 		return;
-	} else {
-		$Dispatcher = new Dispatcher();
-		$Dispatcher->dispatch();
 	}
+
+	App::uses('Dispatcher', 'Routing');
+	$Dispatcher = new Dispatcher();
+	$Dispatcher->dispatch(new CakeRequest());
